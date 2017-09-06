e195e95a0f16cb371c5a3397b608914fb8e426d2
Abhishek Bansal <discover.ab@gmail.com>
2017-09-06 13:58:37 +0530

    + kotlin configuration

---
+++ app/src/main/java/com/moldedbits/showcase/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.moldedbits.showcase
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_app)
+    }
+}
