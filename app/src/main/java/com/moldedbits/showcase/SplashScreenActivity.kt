6c9145cba5a6cae51050cd357e38f1cd3e95496d
Abhishek Bansal <discover.ab@gmail.com>
2017-09-06 21:53:08 +0530

    + splash animations in progress

--- app/src/main/java/com/moldedbits/showcase/SplashScreenActivity.kt
+++ app/src/main/java/com/moldedbits/showcase/SplashScreenActivity.kt
@@ -1,13 +1,43 @@
 package com.moldedbits.showcase
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.Window
+import android.view.WindowManager
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
+import com.moldedbits.showcase.animations.SplashBgSlideDownAnimator
+import kotlinx.android.synthetic.main.activity_example_app.*
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_example_app)
+
+        splashBg.viewTreeObserver.addOnGlobalLayoutListener(object: ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                splashBg.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                YoYo.with(SplashBgSlideDownAnimator())
+                        .duration(1000)
+                        .onEnd {
+                            nameView.visibility = View.VISIBLE
+                            YoYo.with(Techniques.Tada)
+                                    .duration(800)
+                                    .playOn(nameView)
+                            YoYo.with(Techniques.Pulse)
+                                    .duration(400)
+                                    .playOn(logoIv)
+                        }
+                        .playOn(splashBg)
+            }
+        })
     }
 }
